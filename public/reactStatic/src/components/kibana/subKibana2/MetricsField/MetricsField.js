1bdf077281caaf7392e7546856e814cbdc8612fa
john <johlin0207@gmail.com>
2017-10-12 17:26:25 +0800

    complete metrics components

--- public/reactStatic/src/components/kibana/subKibana2/MetricsField/MetricsField.js
+++ public/reactStatic/src/components/kibana/subKibana2/MetricsField/MetricsField.js
@@ -8,11 +8,12 @@ class MetricsField extends Component {
     }
 
     render() {
+        //console.log('&&&&&&&'+JSON.stringify(this.props.content[this.props.index].field));
         return (
             <div className='form-group'>
                 <h5>{this.props.name}</h5>
-                <select className='form-control'>
-                    {this.props.datas.field.map((data,index)=>{
+                <select className='form-control' onChange={this.props.changeHandle}>
+                    {this.props.constructor.field.map((data,index)=>{
                         return <option key={index} value={data}>{data}</option>
                     })}
 
---
+++ public/reactStatic/src/components/kibana/subKibana2/Percents/Percents.js
@@ -0,0 +1,94 @@
+import React, {Component} from 'react'
+import Close from 'react-icons/lib/fa/close'
+import {modifyMetrics2} from "../../../../actions/index";
+import {connect} from 'react-redux'
+
+class Percents extends Component {
+    constructor() {
+        super()
+    }
+
+    delPercent(percentIndex) {
+        //console.log('called del');
+        const {index, name} = this.props;
+        let Percent = this.props.metrics[index].Percents;
+        //console.log('metrics:: ' + newPercent);
+        const newPercent = Percent.filter((item, index) => {
+            return index !== percentIndex
+        });
+        this.props.modifyMetrics(index, name, newPercent)
+    }
+
+    addPercent() {
+        const {index, name} = this.props;
+        let Percent = this.props.metrics[index].Percents;
+        let len = Percent.length;
+        //Percent.push(Percent[len-1]+1);
+        let newPercent = [...Percent, Percent[len - 1] + 1];
+        this.props.modifyMetrics(index, name, newPercent);
+    }
+
+    changeHandle(changeIndex, e) {
+        const {index, name} = this.props;
+        let Percent = this.props.metrics[index].Percents;
+        let newPercent = Percent.map((item, index) => {
+            if(index===changeIndex){
+                item=e.target.value;
+                //删除完某一input框的内容时删除该框
+                if(item===''){
+                    this.delPercent.bind(this, changeIndex)
+                }
+                return item
+            }else{
+                return item
+            }
+        });
+        this.props.modifyMetrics(index, name, newPercent);
+    };
+
+    render() {
+        //console.log('***'+JSON.stringify(this.props.metrics[0].Percents));
+        //[{"field":["age","num"],"Percents":[1,5,25,50,75,95,99],"CustomLabel":""}]
+        //const {index, name} = this.props;
+        //console.log('this.state:: '+JSON.stringify(this.props.metrics[index]));
+        //console.log('this.props:: ' + JSON.stringify(this.props));
+        return (
+            <div className='form-group'>
+                <h5>{this.props.name}</h5>
+                <table>
+                    <tbody>
+                    {this.props.metrics[this.props.index].Percents.map((data, index) => {
+                        return (
+                            <tr key={index}>
+                                <td><input type="text" value={data} className='form-control'
+                                           onChange={this.changeHandle.bind(this, index)}/></td>
+                                <td><Close onClick={this.delPercent.bind(this, index)}/></td>
+                            </tr>
+                        )
+                    })}
+                    </tbody>
+
+                </table>
+                <button onClick={this.addPercent.bind(this)}>Add</button>
+            </div>
+
+        )
+    }
+
+}
+
+const mapStateToProps = (state) => {
+    return {
+        metrics: state.metrics2
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        modifyMetrics: (index, key, value) => {
+            dispatch(modifyMetrics2(index, key, value))
+        }
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Percents);
